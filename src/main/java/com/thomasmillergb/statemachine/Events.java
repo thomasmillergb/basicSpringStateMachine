5afdd37e92840334c9f9ec8f481ee524a5645d84
milleth <thomas.miller@rbs.com>
2016-12-08 16:52:36 +0000

    first commit

---
+++ src/main/java/com/thomasmillergb/statemachine/Events.java
@@ -0,0 +1,11 @@
+package com.thomasmillergb.statemachine;
+
+/**
+ * Created by milleth on 08/12/2016.
+ *
+ * @author milleth
+ */
+public enum  Events
+{
+    E1, E2
+}
---
+++ src/main/java/com/thomasmillergb/statemachine/States.java
@@ -0,0 +1,11 @@
+package com.thomasmillergb.statemachine;
+
+/**
+ * Created by milleth on 08/12/2016.
+ *
+ * @author milleth
+ */
+public enum  States
+{
+    SI, S1, S2
+}
